29e41626e86211198a9d447e96dc01b7aead1055
sakis475 <sakis475@users.noreply.github.com>
2017-03-02 18:40:37 +0200

    Update TweetBeacon.js

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -17,13 +17,13 @@ TweetBeacon = function(tweet) {
   // Set base color depending on sentiment score
   this.color = 0xFFFFFF;
 
-  if (tweet.sentiment.score < -2) {
+  if (tweet.sentiment.score <= -2) {
   this.color = 0xFF0000;
   }
-  else if (tweet.sentiment.score => -2 && tweet.sentiment =< -1) {
+  else if (tweet.sentiment.score > -2 && tweet.sentiment <= -1) {
     this.color = 0x1E90FF;
   }
-  else if (tweet.sentiment.score => -1 && tweet.sentiment =< 0 ) {
+  else if (tweet.sentiment.score > -1 && tweet.sentiment <= 0 ) {
     this.color = 0xFFA500;
   }
   else if (tweet.sentiment > 0 ) {
