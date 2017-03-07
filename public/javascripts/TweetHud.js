f9ca4a7bcb11eb08d51b05cd1fb8169fd3787c84
sakis475 <sakis475@users.noreply.github.com>
2017-03-07 16:59:35 +0200

    Update TweetHud.js

--- public/javascripts/TweetHud.js
+++ public/javascripts/TweetHud.js
@@ -95,7 +95,7 @@ app.controller('TweetHud', function($scope, $resource, $timeout, $rootScope, $ti
   	if (score <= -2) {
   		state = 'veryNegative';
   	}
-  	else if (score > -2 && score < 0) {
+  	else if (score > -2 && score <= 0) {
   		state = 'negative';
   	}
 	else if (score > 0 && score <= 2){
