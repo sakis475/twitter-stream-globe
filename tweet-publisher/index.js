54e4d3209885fd6b05b9d1acb4147dcef9b4aeea
sakis475 <sakis475@users.noreply.github.com>
2017-03-18 15:48:06 +0200

    Update index.js

--- tweet-publisher/index.js
+++ tweet-publisher/index.js
@@ -33,7 +33,7 @@ TweetPublisher.start = function () {
 	if (!stream) {
 
 		// Connect to stream and filter by a geofence that is the size of the Earth
-		stream = twitter.stream('statuses/filter', { locations: '-180,-90,180,90' });
+		stream = twitter.stream('statuses/filter', { locations: '-90,-90,180,90' });
 
 		// When Tweet is received only process it if it has geo data
 		stream.on('tweet', function (tweet) {	
