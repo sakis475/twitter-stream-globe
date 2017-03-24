b7594be5c5d741617a7d0a3888903fa99195b33b
sakis475 <sakis475@users.noreply.github.com>
2017-03-24 11:13:03 +0200

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -58,7 +58,7 @@
 	  var shader = Shaders.earth;
 	  var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-	  uniforms['texture'].value = THREE.ImageUtils.loadTexture('/images/earthOnPaper.jpg');
+	  uniforms['texture'].value = THREE.ImageUtils.loadTexture('/images/original.jpg');
 
 	  var material = new THREE.ShaderMaterial({
 	    uniforms: uniforms,
